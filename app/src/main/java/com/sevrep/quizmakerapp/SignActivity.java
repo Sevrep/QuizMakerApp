06e36232b00763ce3c31adf91a1fa40c7b0f0721
Ivan <sevrep@yahoo.com>
2020-10-31 04:46:36 +0800

    Added sign up adding account function done.

--- app/src/main/java/com/sevrep/quizmakerapp/SignActivity.java
+++ app/src/main/java/com/sevrep/quizmakerapp/SignActivity.java
@@ -1,14 +1,26 @@
 package com.sevrep.quizmakerapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.content.Intent;
-import android.os.Bundle;
+import com.sevrep.quizmakerapp.singleton.DatabaseHelper;
 
 import java.util.Objects;
 
 public class SignActivity extends AppCompatActivity {
+
+    private EditText edtFullname, edtUsername, edtPassword, edtConfirm;
+    private Spinner sprType;
+
+    DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +33,71 @@ public class SignActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
+        edtFullname = findViewById(R.id.edtFullname);
+        edtUsername = findViewById(R.id.edtUsername);
+        edtPassword = findViewById(R.id.edtPassword);
+        edtConfirm = findViewById(R.id.edtConfirm);
+        sprType = findViewById(R.id.sprType);
+        Button btnCreate = findViewById(R.id.btnCreate);
+        btnCreate.setOnClickListener(v -> validateEntries());
+
+        databaseHelper = new DatabaseHelper(this);
+    }
+
+    private void validateEntries() {
+        String fullname = edtFullname.getText().toString().trim();
+        String username = edtUsername.getText().toString().trim();
+        String password = edtPassword.getText().toString().trim();
+        String confirm = edtConfirm.getText().toString().trim();
+        String type = sprType.getSelectedItem().toString();
+
+        if (TextUtils.isEmpty(fullname)) {
+            edtFullname.setError("Enter full name.");
+            return;
+        }
+        if (TextUtils.isEmpty(username)) {
+            edtUsername.setError("Enter username.");
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            edtPassword.setError("Enter password.");
+            return;
+        }
+        if (TextUtils.isEmpty(confirm)) {
+            edtConfirm.setError("Enter confirmation password.");
+            return;
+        }
+        if (type.equals("ACCOUNT TYPE")) {
+            customToast("Please choose and account type.");
+        } else {
+            if(databaseHelper.checkUsernameDuplicate(username)) {
+                customToast("Username already taken!");
+            } else if (databaseHelper.checkFullnameDuplicate(fullname)) {
+                customToast("You already have an account!");
+            } else {
+                databaseHelper.createUser(fullname, username, password, type);
+                customToast("Account saved!.");
+                goToMain();
+            }
+        }
     }
 
     @Override
     public boolean onSupportNavigateUp() {
-        onBackPressed();
+        goToMain();
         return super.onSupportNavigateUp();
     }
 
     @Override
     public void onBackPressed() {
+        goToMain();
+    }
+
+    private void customToast(String mensahe) {
+        Toast.makeText(this, mensahe, Toast.LENGTH_SHORT).show();
+    }
+
+    public void goToMain() {
         Intent iMain = new Intent(this, MainActivity.class);
         startActivity(iMain);
         finish();
---
+++ app/src/main/java/com/sevrep/quizmakerapp/singleton/DatabaseHelper.java
@@ -0,0 +1,95 @@
+package com.sevrep.quizmakerapp.singleton;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import android.widget.Toast;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    Context c;
+
+    private static final String DB_NAME = "myDB.db";
+    private static final String TABLE_USERS = "tblUserAccount";
+
+    public DatabaseHelper(Context context){
+        super(context,DB_NAME,null,6);
+        c = context;
+    }
+
+    public void onCreate(SQLiteDatabase db){
+        try {
+            String sqlQuery = "CREATE TABLE " + TABLE_USERS + "(fullname text primary key, username text, password text, type text)";
+            db.execSQL(sqlQuery);
+            Toast.makeText(c, "Users table created successfully.", Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            Log.e("DATABASEHELPER ", "Users table creation error.", e);
+        }
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_USERS);
+        onCreate(db);
+    }
+
+    /**USER*/
+    public void createUser(String fullname, String username, String password, String type){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("fullname", fullname);
+        cv.put("username", username);
+        cv.put("password", password);
+        cv.put("type", type);
+        db.insert(TABLE_USERS,null,cv);
+    }
+
+    public boolean checkUsernameDuplicate(String username) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String sqlQuery = "SELECT * FROM " +TABLE_USERS+ " WHERE username = '" + username + "' ";
+        Cursor c = db.rawQuery(sqlQuery,null);
+        if(c.getCount() != 0){
+            return true;
+        }
+        c.close();
+        return false;
+    }
+
+    public boolean checkFullnameDuplicate(String fullname) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String sqlQuery = "SELECT * FROM " +TABLE_USERS+ " WHERE fullname = '" + fullname + "' ";
+        Cursor c = db.rawQuery(sqlQuery,null);
+        if(c.getCount() != 0){
+            return true;
+        }
+        c.close();
+        return false;
+    }
+
+    public boolean loginUser(String username, String password, String type){
+        SQLiteDatabase db= this.getReadableDatabase();
+        String sqlQuery = "SELECT * FROM " +TABLE_USERS+ " WHERE username = '" +username+ "' AND password = '" +password+ "' AND type = '" +type+ "' ";
+        Cursor c = db.rawQuery(sqlQuery,null);
+        if(c.getCount() != 0){
+            return true;
+        }
+        c.close();
+        return false;
+    }
+
+    public void updateUserData(String fullname, String username, String password){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("username",username);
+        cv.put("password",password);
+        db.update(TABLE_USERS,cv,"fullname = '" + fullname + "' ",null);
+    }
+
+    public void deleteUserData(String fullname){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_USERS,"fullname = '"+fullname+"' ",null);
+    }
+
+}
