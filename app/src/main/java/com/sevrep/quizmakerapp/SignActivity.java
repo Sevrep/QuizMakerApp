21f7c4496eb9009be77745a839e5d078837de5f3
Ivan <sevrep@yahoo.com>
2020-10-31 03:44:44 +0800

    Added sign up design.

--- app/src/main/java/com/sevrep/quizmakerapp/SignActivity.java
+++ app/src/main/java/com/sevrep/quizmakerapp/SignActivity.java
@@ -1,9 +1,12 @@
 package com.sevrep.quizmakerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Objects;
 
 public class SignActivity extends AppCompatActivity {
 
@@ -11,6 +14,19 @@ public class SignActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign);
+
+        Toolbar toolbar = findViewById(R.id.toolbar_sign);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
     }
 
     @Override
