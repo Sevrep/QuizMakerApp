d976ee9c7b9c55f5cfe71c32bab615b2f94e8a96
Ivan <sevrep@yahoo.com>
2020-11-01 10:01:29 +0800

    TeacherActivity design.

--- app/src/main/java/com/sevrep/quizmakerapp/TeacherActivity.java
+++ app/src/main/java/com/sevrep/quizmakerapp/TeacherActivity.java
@@ -3,9 +3,6 @@ package com.sevrep.quizmakerapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
@@ -14,13 +11,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.sevrep.quizmakerapp.singleton.SharedPrefHandler;
 
-public class TeacherActivity extends AppCompatActivity implements View.OnClickListener {
+public class TeacherActivity extends AppCompatActivity {
 
-    private FloatingActionButton fab_main, fab1_trueorfalse, fab2_multiple;
-    private Animation fab_open, fab_close, fab_clock, fab_anticlock;
-    TextView textview_trueorfalse, textview_multiple;
-
-    Boolean isOpen = false;
+    private FloatingActionButton fab_main;
 
     SharedPrefHandler sharedPrefHandler;
 
@@ -30,20 +23,9 @@ public class TeacherActivity extends AppCompatActivity implements View.OnClickLi
         setContentView(R.layout.activity_teacher);
 
         fab_main = findViewById(R.id.fab);
-        fab1_trueorfalse = findViewById(R.id.fab1_trueorfalse);
-        fab2_multiple = findViewById(R.id.fab2_multiple);
+        fab_main.setOnClickListener(v -> {
 
-        fab_close = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close);
-        fab_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
-        fab_clock = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_rotate_clock);
-        fab_anticlock = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_rotate_anticlock);
-
-        textview_trueorfalse = findViewById(R.id.textview_trueorfalse);
-        textview_multiple = findViewById(R.id.textview_multiple);
-
-        fab_main.setOnClickListener(this);
-        fab1_trueorfalse.setOnClickListener(this);
-        fab2_multiple.setOnClickListener(this);
+        });
 
         sharedPrefHandler = new SharedPrefHandler(this);
     }
@@ -63,38 +45,6 @@ public class TeacherActivity extends AppCompatActivity implements View.OnClickLi
                     startActivity(iMain);
                     this.finish();
                 }).create().show();
-    }
-
-    @Override
-    public void onClick(View v) {
-        int objectId = v.getId();
-        if (objectId == R.id.fab) {
-            if (isOpen) {
-                textview_trueorfalse.setVisibility(View.INVISIBLE);
-                textview_multiple.setVisibility(View.INVISIBLE);
-                fab2_multiple.startAnimation(fab_close);
-                fab1_trueorfalse.startAnimation(fab_close);
-                fab_main.startAnimation(fab_anticlock);
-                fab2_multiple.setClickable(false);
-                fab1_trueorfalse.setClickable(false);
-                isOpen = false;
-            } else {
-                textview_trueorfalse.setVisibility(View.VISIBLE);
-                textview_multiple.setVisibility(View.VISIBLE);
-                fab2_multiple.startAnimation(fab_open);
-                fab1_trueorfalse.startAnimation(fab_open);
-                fab_main.startAnimation(fab_clock);
-                fab2_multiple.setClickable(true);
-                fab1_trueorfalse.setClickable(true);
-                isOpen = true;
-            }
-        } else if (objectId == R.id.fab1_trueorfalse) {
-            customToast("Go to true or false activity.");
-        } else if (objectId == R.id.fab2_multiple) {
-            customToast("Go to multiple choice activity.");
-        } else {
-            throw new IllegalStateException("Unexpected value: " + v.getId());
-        }
     }
 
     public void customToast(String mensahe) {
